14e3bbddf7df11a8e46e4f85526964bc6f90c324
SeongYong <yyyy7246@naver.com>
2022-08-16 18:03:57 +0900

    feat: ✨ account drf 구현

---
+++ unplug/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+]
